1c971224d3761c05752e5ff7bd6355f542693b0c
Malachi-T <mbabylight@gmail.com>
2022-12-06 10:54:26 -0500

    first commit

---
+++ api.js
@@ -0,0 +1,36 @@
+document.getElementById('random').addEventListener('click', randomCocktail)
+document.getElementById('drinkSearchBtn').addEventListener('click', searchCocktail)
+
+function searchCocktail (){
+  let drink = document.getElementById('drinkSearch').value
+  drink = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=' + drink
+  console.log(drink)
+  fetch(drink)
+  .then(res => res.json())
+  .then(data => {
+    console.log(data.drinks[0])
+    document.querySelector('img').src = data.drinks[0].strDrinkThumb
+    document.getElementById('drinkName').textContent = data.drinks[0].strDrink
+  })
+}
+
+
+function test(){
+  console.log('tests works')
+}
+
+function randomCocktail (){ 
+  fetch("https://www.thecocktaildb.com/api/json/v1/1/random.php")
+    .then(res => res.json())
+    .then(data => {
+      console.log(data.drinks[0])
+      document.querySelector('img').src = data.drinks[0].strDrinkThumb
+      document.getElementById('drinkName').textContent = data.drinks[0].strDrink
+    })
+    .catch(err => {
+      console.log(`error ${err}`)
+    })
+
+}
+
+// www.thecocktaildb.com/api/json/v1/1/search.php?s=
