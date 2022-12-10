c6be45dee9e8bc0c3209eff86c573678dc464931
Malachi-T <mbabylight@gmail.com>
2022-12-09 22:16:28 -0500

    added functionality for ingredient list

--- api.js
+++ api.js
@@ -15,6 +15,13 @@ function searchCocktail (){
     console.log(data.drinks[0])
     document.querySelector('img').src = data.drinks[0].strDrinkThumb
     document.getElementById('drinkName').textContent = data.drinks[0].strDrink
+    document.getElementById('ingredients').textContent = data.drinks[0].strIngredient1
+  })
+  .catch(err => {
+    console.log(`error ${err}`)
+    document.getElementById('drinkName').textContent = 'That isnt in the data base'
+    return
+
   })
 }
 
@@ -24,12 +31,29 @@ function test(){
 }
 
 function randomCocktail (){ 
+  let ings = []
   fetch("https://www.thecocktaildb.com/api/json/v1/1/random.php")
     .then(res => res.json())
     .then(data => {
       console.log(data.drinks[0])
       document.querySelector('img').src = data.drinks[0].strDrinkThumb
       document.getElementById('drinkName').textContent = data.drinks[0].strDrink
+      console.log(data.drinks.keys(),'test')
+      for (const key in data.drinks[0]) {
+        if(data.drinks[0][key] !== null){
+          if(key.startsWith('strIng')){
+            ings.push(data.drinks[0][key])
+          }
+          console.log(`${key} and ${data.drinks[0][key]}`)
+        }
+      }
+      console.log(ings)
+      let ingList = document.getElementById('ingUL')
+      ings.forEach( x => {
+        let li = document.createElement("li")
+        li.innerText = x
+        ingList.appendChild(li)
+      })
     })
     .catch(err => {
       console.log(`error ${err}`)
